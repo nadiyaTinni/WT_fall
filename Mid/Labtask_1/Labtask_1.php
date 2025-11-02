c633f2e94f4d771b618b7d2a769a459c7d1be6c6
nadiyaTinni <nadiyanewaztinni@gmail.com>
2025-11-02 21:05:10 +0600

    first commit

---
+++ Mid/Labtask_1/Labtask_1.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Clinic Patient Registration</title>
+ 
+</head>
+
+<body class="body">
+  
+  <center>
+  <h1 style="color:Blue;">Clinic Patient Registration</h1>
+  </center>
+  <form>
+    <table>
+      <tr>
+        <td>Full Name</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>Age</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>Phone Number</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>Email Adress</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+       <td><select>
+          <option>Insurance Provider :</option>
+          <option>Provide X</option>
+          <option>Provide Y</option>
+          <option>Provide Z</option>
+        </select></td> 
+
+      
+      </tr> 
+     
+      
+  </table>
+ 
+  </form>
+  <table>
+    <form>
+       <center>
+  <h1 style="color:Blue;">Additional Information</h1>
+    </center>
+    <tr>
+        <td>User Name</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>Confirm Password</td>
+        <td><input type="text" ></td>
+
+       
+      </tr>
+      <tr>
+        <td>
+            <input type="submit" name="submit">
+      </td>
+      </tr>
+      
+
+
+    </form>
+  </table>
+  
+</body>
